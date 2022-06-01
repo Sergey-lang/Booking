fa4f7e22341b350a333a2541e5fe21ab750dc677
Sergey <sergei.kuharyonok@yandex.by>
2022-06-01 22:56:18 +0300

    Hotels CRUD

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,6 +14,42 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.status(200).json(updatedHotel);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deteted');
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
 router.get('/register', (req, res) => {
   res.send('Hello register');
 });
