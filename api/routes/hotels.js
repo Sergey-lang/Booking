daf35a44a19924b3c9a2e71c9b852d4cfedfdda2
Sergey <sergei.kuharyonok@yandex.by>
2022-06-01 23:26:19 +0300

    Error handling

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -41,12 +41,12 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const hotels = await Hotel.find();
     res.status(200).json(hotels);
   } catch (e) {
-    res.status(500).json(e);
+    next(e)
   }
 });
 
